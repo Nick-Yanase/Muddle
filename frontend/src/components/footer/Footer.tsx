f9f931a221e3aee5171a739bb05568a2667f1ca8
cafeeb0m <kauayuuki10@gmail.com>
2024-11-05 22:40:34 -0300

    update footer

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -5,10 +5,10 @@ import SocialMedia from "./SocialMedia";
 
 export default function Footer() {
   return(
-    <footer className="w-screen bg-darkCyan text-white ">
+    <footer className="w-screen bg-cyan50 text-white ">
       <div className="w-4/5 h-2/5 mx-auto p-8">
         <LogoFooter />
-        <div className="flex gap-8 w-full h-full">
+        <div className="flex justify-between w-full h-full">
           <ContactWays />
           <Links />
           <SocialMedia />
